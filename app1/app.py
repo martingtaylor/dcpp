c84184953cf39c97b7f2afe5db633b2487f214ec
martin-taylor <martin.g.taylor@btinternet.com>
2021-06-07 08:47:02 +0100

    Finished work on APP1

--- app1/app.py
+++ app1/app.py
@@ -7,18 +7,18 @@ from os import getenv
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-service2 = 'http://serv2 :5002'
-service3 = 'http://serv3:5003'
-service4 = 'http://serv4:5004'
+service2 = 'http://app2:5002'
+service3 = 'http://app3:5003'
+service4 = 'http://app4:5004'
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
 
-class record_fires(db.Model):
-    id              = db.Column(db.Integer,     primary_key=True)
-    velocity        = db.Column(db.Integer,     nullable=False)
-    elevation       = db.Column(db.Integer,     nullable=False)
-    result          = db.Column(db.Integer,     nullable=False)
+#class record_fires(db.Model):
+#    id              = db.Column(db.Integer,     primary_key=True)
+#    velocity        = db.Column(db.Integer,     nullable=False)
+#    elevation       = db.Column(db.Integer,     nullable=False)
+#    result          = db.Column(db.Integer,     nullable=False)
 
 @app.route('/')
 def index():
@@ -27,16 +27,22 @@ def index():
 @app.route('/fire', methods=['GET','POST'])
 def home():
     # Get data from App2 and App3
-    velocity    = requests.get(service2)
-    elevation   = requests.get(service3)
-    data    = [power.text, angle.text]
-    single = ','.join(data)
-    outcome = requests.post(service4, data=single)
-    new_kick = Newoutcomes(kick_power=power.text, kick_angle=angle.text, kick_outcome=outcome.text)
-    db.session.add(new_kick)
-    db.session.commit()
-    return render_template('home.html', power=power, angle=angle, outcome=outcome) 
+    muzzle_velocity = requests.get(service2)
+    elevation_angle = requests.get(service3)
+
+    # Build into data to send to App4
+    data            = [elevation_angle.text, muzzle_velocity.text]
+    single          = ','.join(data)
+
+    # Get result back
+    result = requests.post(service4, data=single)
+
+    # Write to database
+    #new_kick = Newoutcomes(kick_power=power.text, kick_angle=angle.text, kick_outcome=outcome.text)
+    #db.session.add(new_kick)
+    #db.session.commit()
+
+    return render_template('index.html', velocity=muzzle_velocity, angle=elevation_angle, outcome=result) 
 
 
-if __name__=='__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__=='__main__': app.run(host='0.0.0.0', port=5000, debug=True)
