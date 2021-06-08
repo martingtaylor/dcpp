29d2186d1a24af9e12b49de8cddce9f01f4b7d98
martin-taylor <martin.g.taylor@btinternet.com>
2021-06-08 16:31:18 +0100

    Database

--- app1/app.py
+++ app1/app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import random
 import requests
 from os import getenv
+from sqlalchemy import desc
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
@@ -38,7 +39,7 @@ def fire():
     result = requests.post(app4, data=single)
 
     # Query Database before adding
-    all_firings = record_fires.query.with_entities(record_fires.result).all()
+    all_firings = record_fires.query.with_entities(record_fires.result).order_by(desc(record_fires.id)).limit(50).all()
 
     # Write to database
     firing = record_fires(velocity=muzzle_velocity.text, elevation=elevation_angle.text, result=result.text)
