b16ea424076479cd88933e1303b740056dfd1eef
martin-taylor <martin.g.taylor@btinternet.com>
2021-06-11 14:57:17 +0100

    Minor Changes

--- app3/tests/test_app3.py
+++ app3/tests/test_app3.py
@@ -10,7 +10,7 @@ class TestBase(TestCase):
 class TestPowerCall(TestBase):
     def test_app3(self):
         response = self.client.get(url_for('velocity'))
-        list_angles = "20,30,40,50,60,70"
         self.assert200(response)
-        self.assertIn(response.data.decode('utf-8'), "90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110")
+        self.assertIn(int(response.data.decode('utf-8')), range(90,120))
+
         
